983c6c1ddc17230f9496127102e6bdc882f81fb0
mbreitner <84791455+mbreitner@users.noreply.github.com>
2021-08-29 12:07:32 -0400

    adding in filtering functions

--- js/app.js
+++ js/app.js
@@ -36,5 +36,39 @@ function buildTable(data) {
             }
         );
     });
-}
+};
+
+// create a function to begin adding filters to our page
+//add a date function so the page can be filtered by date
+function handleClick() {
+    // .select will select the very first element that matches our string selector "#datetime"
+    // d3.select("#datetime") we're tellig d3 to look for the #datetime ID in the HTML tags
+    // by chaining .property("value") we're telling d3 to actually grab the information and hold it in the date value
+    let date = d3.select("#datetime").property("value");
+    // By setting the filteredData variable to our raw data, we're basically using it as a blank slate
+    // this is the default filter
+    //If no date has been entered as a filter, then all of the data will be returned instead.
+    let filteredData = tableData;
+    //if a (date is entered) {
+        // filter the default data to show only the date entered
+    //};
+    if (date) {
+        // this line applies the filter to the tabledata
+        // the triple === tests for equality, meaning the data in the table has to match the ffilter exactly
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // rebuild the table using the filtered data
+    // if no date was entered, then filteredData will just be the original tableData
+    buildTable(filteredData);
+};
+
+// listen for the click from the handleClick function so the code knows a click happened
+d3.selectAll("filter-btn").on("click", handleClick);
+// the selector string "filter-btn" is the html ID that tells our code a button has been clicked
+// .on("click", handleClick); is telling D3 to execute the handleClick function when the button is clicked
+
+//build the final table
+buildTable(tableData);
+
 
